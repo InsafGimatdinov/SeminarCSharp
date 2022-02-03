229a4c380f6e123bec46b901089043198a80f8fa
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 14:01:38 +0300

    Add Task 014 Third FigureNumbers

---
+++ 014_ThirdFugureNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет.
+
+System.Console.WriteLine("Введите любое целое число ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+// Задали условие при котором третья цифра может быть только в этих пределах
+if ((number > 99) && (number < 1000))
+{
+    System.Console.WriteLine(number % 10);
+    System.Console.ReadKey();
+}
+else
+{
+    System.Console.Write("В этом числе нет третьей цифры ");
+    System.Console.ReadKey();
+}
