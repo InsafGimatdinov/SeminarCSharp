67809bbac0b3d2b0e5f8024871d8f641c820ac85
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:51:16 +0300

    Add Task Day Of Week

---
+++ 003_NumberDayOFWeek/Program.cs
@@ -0,0 +1,16 @@
+﻿// 3. По заданному номеру дня недели вывести его название
+
+//Зададим запрос числа из консоли
+Console.WriteLine("Введите номер дня недели от 1 до 7");
+
+//Конвертирование типа данных
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+//Задаём условие при котором число дает день недели
+if (DayOfWeek == 1) System.Console.WriteLine("Monday");
+if (DayOfWeek == 2) System.Console.WriteLine("Tuesday");
+if (DayOfWeek == 3) System.Console.WriteLine("Wednesday");
+if (DayOfWeek == 4) System.Console.WriteLine("Thursday");
+if (DayOfWeek == 5) System.Console.WriteLine("Friday");
+if (DayOfWeek == 6) System.Console.WriteLine("Saturday");
+if (DayOfWeek == 7) System.Console.WriteLine("Sunday");
