90c7969b66ef48827eb86d200c2f393f007a221c
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:50:12 +0300

    Add 002 Task Max Min Two Numbers

---
+++ 002_MaxMinTwoNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿// 2.Даны два числа. Показать большее и меньшее число
+
+//Вводим две переменные
+int numberA = new Random().Next(1, 20);
+int numberB = new Random().Next(1, 20);
+int max = numberA;
+int min = numberA;
+
+//Задаём условие для нахождения минимального и максимального
+if (numberB > max) 
+{
+    max = numberB;
+    min = numberA;
+}
+else
+{
+   max = numberA;
+   min = numberB;
+}
+
+System.Console.WriteLine($"max = {max}");
+System.Console.WriteLine($"min = {min}");
