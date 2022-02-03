40b6b23d2a7218aaa81635e9089a1768923f0557
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-04 01:42:54 +0300

    add example about show array zero and one

---
+++ 032_ShowArray8NumberAndZeroOne/Program.cs
@@ -0,0 +1,22 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран.
+
+void FillArray(int[] arrayToFill)
+{
+    for (int i = 0; i < arrayToFill.Length; i++)
+    {
+        arrayToFill[i] = new Random().Next(0, 2);
+    }
+}
+
+void ShowArray(int[] arrayToShow)
+{
+    for (int i = 0; i < arrayToShow.Length; i++)
+    {
+        Console.Write($"[{i}]:{arrayToShow[i]}");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[8];
+FillArray(array);
+ShowArray(array);
