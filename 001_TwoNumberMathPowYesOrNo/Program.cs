ad2dfe78257d8f4617ec6b8c8903e27b1bb74f66
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:49:16 +0300

    Add 001 Task Two Numbers Math Pow

---
+++ 001_TwoNumberMathPowYesOrNo/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1.По двум заданным числам проверять является ли первое квадратом второго
+
+//Вводим две переменные:
+int numberA = 36;
+int numberB = new Random().Next(1, 50);
+
+System.Console.WriteLine(numberA);
+System.Console.WriteLine(numberB);
+
+//Задаём условие проверки
+if (numberA == Math.Pow(numberB, 2))
+{
+    System.Console.WriteLine("True");
+}
+else
+{
+    System.Console.WriteLine("False");
+} 
+
+
+
