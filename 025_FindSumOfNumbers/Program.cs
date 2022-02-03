01ae02ea3320947a1c6edf37fe5f89b202038396
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 14:09:03 +0300

    Add 025 Task About Find Sum Of 1 A

---
+++ 025_FindSumOfNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// 25. Найти сумму чисел от 1 до А.
+
+// Ввели число с консоли.
+System.Console.WriteLine("Введите число А ");
+long numberA = Convert.ToInt64(System.Console.ReadLine());
+
+DateTime dt = DateTime.Now; // Текущее время.
+// Ввели некую переменную sum и присвоили ей значение 0.
+long sum = 0;
+
+// Воспользовались циклом for.
+// for (long i = 1; i <= numberA ; i++)
+// {
+//     sum = sum + i;  
+// }
+sum = numberA * (numberA + 1) / 2;
+System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+System.Console.WriteLine(sum);
