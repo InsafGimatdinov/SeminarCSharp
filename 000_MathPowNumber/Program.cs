1793c5d1c4358491fcc27b9952d4aaeb7c23ead3
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:47:45 +0300

    Add Task 000 Of Math Pow Of Numbers

---
+++ 000_MathPowNumber/Program.cs
@@ -0,0 +1,9 @@
+﻿// 0.Вывести квадрат числа
+
+//Ввели переменную number
+int number = new Random().Next(1, 50);
+
+//Выводи сгенерированное число
+System.Console.WriteLine(number);
+//Выводим квадрат числа
+System.Console.WriteLine(Math.Pow(number, 2));
