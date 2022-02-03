c6ce1e4d699d1dbb4ba304ae99362a7e0f75fbe1
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:52:05 +0300

    Add Task Of Min Three Numbers

---
+++ 004_MinOfThreeNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// 3. Найти минимальное из трех чисел
+
+//Зададимся переменными
+int A = new Random().Next(1, 50);
+int B = new Random().Next(1, 40);
+int C = new Random().Next(1, 30);
+
+//Для проверки работоспособности программы выводим то, что получается в наборе случайных чисел
+System.Console.WriteLine(A);
+System.Console.WriteLine(B);
+System.Console.WriteLine(C);
+
+//Изначально присваиваем минимум первой переменной
+int min = A;
+
+//Задаём условие проверки на минимум
+if (B < min) min = B;
+if (C < min) min = C;
+
+//Выводим минимальное значение из 3
+Console.WriteLine($"min = {min}");
