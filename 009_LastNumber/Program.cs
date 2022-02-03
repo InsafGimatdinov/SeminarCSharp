e5b59333f1376e93d176425c58e1fde5a7f97e78
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:56:34 +0300

    Add Examples About Last Numbers

---
+++ 009_LastNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+// int number = 387;
+
+System.Console.WriteLine("Введите число трёхзначное");
+int number = Convert.ToInt32(System.Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    System.Console.WriteLine(number % 10);
+}
+else
+{
+    System.Console.WriteLine("Введено некорректное значение");
+}
