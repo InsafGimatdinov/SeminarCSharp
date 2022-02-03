2a73575633755c38515501864994b973dcb0a573
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:53:53 +0300

    Add Example Even or Uneven

---
+++ 006_ChetnoeOrNo/Program.cs
@@ -0,0 +1,16 @@
+﻿// 6. Выяснить является ли число чётным
+
+//Сделаем запрос ввода числа с консоли
+System.Console.WriteLine("Введите любое число");
+
+//Введем переменную
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
