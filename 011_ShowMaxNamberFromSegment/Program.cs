f6caa1e693252ea243e3e0cc6dd204dee65bbfef
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:58:19 +0300

    Add Task Show Max Number From Segment

---
+++ 011_ShowMaxNamberFromSegment/Program.cs
@@ -0,0 +1,23 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+//Задали отрезок для числа
+int number = new Random().Next(10, 99);
+
+Console.WriteLine(number);
+
+//Расписаны варианты для нахождения цифр в числе
+int result = number % 10;
+int result1 = number / 10;
+int result2 = result1 % 10;
+
+int max = result;
+
+//Задали условие для нахождения максимальной цифры
+if (result2 > result) 
+{
+    Console.WriteLine($"max = {result2}");
+}
+else
+{
+    Console.WriteLine($"max = {result}");
+} 
