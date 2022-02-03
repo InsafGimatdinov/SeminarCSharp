3d69d87f1ea5d9a220c0068eeeeb1150692f0f03
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 14:07:29 +0300

    Add 023 Task About Show Table Math Pow

---
+++ 023_ShowTableMathPow/Program.cs
@@ -0,0 +1,16 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N.
+
+// Ввели число с консоли.
+System.Console.WriteLine("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+// Для решения подобных задач чаще всего используется цикл for.
+for (int i = 0; i * i <= N; i++)
+{
+    Console.WriteLine($"[{i}] = {i * i}, ");
+    if (i % 5 == 0)
+    {
+        System.Console.WriteLine();
+    }
+}
