45c6e235fb3698a1d55a718bfdc0a049a9a81225
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 14:14:26 +0300

    Add task About Write Program Binary Numbers

---
+++ 043_WriteProgramBinarySysten/Program.cs
@@ -0,0 +1,6 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное.
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string binary = Convert.ToString(number, 2);
+System.Console.WriteLine($"Перевод десятичного числа {number} в двоичное {binary}");
