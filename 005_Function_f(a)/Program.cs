8b93be1f405956d8cc559d818d05309c6007a784
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 13:52:51 +0300

    Add Function Task

---
+++ 005_Function_f(a)/Program.cs
@@ -0,0 +1,18 @@
+﻿// 6. Написать программу вычисления значения функции y = f(a) = (1 + 1 / a) ^ a
+
+System.Console.WriteLine("введите число а");
+double a = Convert.ToDouble(System.Console.ReadLine());
+double y = 0;
+if (a == 0)
+{
+    System.Console.WriteLine("Error uncorrect number");
+}
+else
+{
+    y = Math.Pow((1+1/a), a);
+    System.Console.WriteLine($"y = {y}");
+}
+
+// double number = 2;
+// double result = Math.Pow(number, 3);
+// System.Console.WriteLine(result);
