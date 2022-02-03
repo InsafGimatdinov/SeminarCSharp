07935cb1c62d1b0531c5f9eb968f47d40a0fd040
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2022-02-03 14:08:20 +0300

    Add Task Find Math Pow 3

---
+++ 024_FindMathPow3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 24. Найти кубы чисел от 1 до N.
+
+// Ввели число с консоли.
+System.Console.WriteLine("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+// Для решения подобных задач чаще всего используется цикл for.
+for (int i = 0; i * i * i <= N; i++)
+{
+    Console.WriteLine($"[{i}] = {i * i * i}, ");
+    if (i % 5 == 0)
+    {
+        System.Console.WriteLine();
+    }
+}
